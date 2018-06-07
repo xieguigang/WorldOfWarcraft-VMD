39a83ef00c5196e14ca2e67dda4f3807e5091096
xieguigang <xie.guigang@gcmodeller.org>
2018-06-08 01:54:34 +0800

    WMV xml model

--- WMV/ModelInfo.xml.vb
+++ WMV/ModelInfo.xml.vb
@@ -1,3 +1,25 @@
-﻿Public Class ModelInfo
+﻿Imports System.Runtime.CompilerServices
+Imports System.Text
+Imports System.Xml.Serialization
+Imports Microsoft.VisualBasic.Text
 
+''' <summary>
+''' ``m2`` model info
+''' </summary>
+<XmlType("m2")> Public Class ModelInfo
+
+    Public Property info As info
+    Public Property header As header
+
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    Public Shared Function Load(path$, Optional encoding As Encoding = Nothing) As ModelInfo
+        Return path.ReadAllText(encoding Or UTF8).CreateObjectFromXmlFragment(Of ModelInfo)
+    End Function
 End Class
+
+Public Class info
+    Public Property modelname As String
+End Class
+
+
+
---
+++ WMV/SkeletonAndAnimation.vb
@@ -0,0 +1,89 @@
+﻿Imports System.Xml
+Imports System.Xml.Schema
+Imports System.Xml.Serialization
+
+Public Class Sequences : Inherits List(Of Integer)
+
+    <XmlElement>
+    Public Property Sequence As Integer()
+End Class
+
+Public Class SkeletonAndAnimation
+
+    Public Property GlobalSequences As Sequences
+    Public Property Animations As Animations
+    Public Property AnimationLookups As AnimationLookups
+
+End Class
+
+Public Class List(Of T)
+
+    <XmlAttribute>
+    Public Property size As Integer
+End Class
+
+Public Class Animations : Inherits List(Of Animation)
+
+    <XmlElement("Animation")>
+    Public Property Animations As Animation()
+End Class
+
+Public Class Animation
+
+    <XmlAttribute>
+    Public Property id As Integer
+
+    Public Property animID As Integer
+    Public Property animName As String
+    Public Property length As Integer
+    Public Property moveSpeed As Integer
+    Public Property Flags As Integer
+    Public Property probability As Integer
+    Public Property d1 As Integer
+    Public Property d2 As Integer
+    Public Property playSpeed As Integer
+    Public Property boxA As String
+    Public Property boxB As String
+    Public Property rad As Double
+    Public Property NextAnimation As Integer
+    Public Property Index As Integer
+
+End Class
+
+Public Class AnimationLookup
+    <XmlAttribute>
+    Public Property id As Integer
+    <XmlText>
+    Public Property value As Integer
+End Class
+
+Public Class AnimationLookups : Inherits List(Of AnimationLookup)
+
+    <XmlElement("AnimationLookup")>
+    Public Property AnimationLookups As AnimationLookup()
+End Class
+
+Public Class Bone
+    Public Property keyboneid As Integer
+    Public Property billboard As Integer
+    Public Property parent As Integer
+    Public Property geoid As Integer
+    Public Property unknown As Integer
+End Class
+
+Public Class Vector : Implements IXmlSerializable
+
+    Public Property pivot As Double()
+
+    Public Sub ReadXml(reader As XmlReader) Implements IXmlSerializable.ReadXml
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Sub WriteXml(writer As XmlWriter) Implements IXmlSerializable.WriteXml
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Function GetSchema() As XmlSchema Implements IXmlSerializable.GetSchema
+        Throw New NotImplementedException()
+    End Function
+End Class
