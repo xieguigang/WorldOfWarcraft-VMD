43c21b55d6b4158a33bc867d2945dc0a77caf93d
关倩倩 <amethyst.asuka@gcmodeller.org>
2018-06-08 22:57:57 +0800

    add vmd reader test

---
+++ test/Module1.vb
@@ -0,0 +1,17 @@
+﻿Imports MikuMikuDance.File.VMD
+
+Module Module1
+
+    Sub Main()
+        Call vmdReaderTest()
+
+    End Sub
+
+    Sub vmdReaderTest()
+
+        Dim path = "C:\Users\Evia\source\repos\VMD\DATA\MOTION.vmd"
+        Dim vmd = Reader.OpenAuto(path)
+
+        Pause()
+    End Sub
+End Module
