7e750eee6112638e759d5d80b49216b88e9f566b
关倩倩 <amethyst.asuka@gcmodeller.org>
2018-06-08 23:38:53 +0800

    test on dump xml

--- test/Module1.vb
+++ test/Module1.vb
@@ -12,6 +12,8 @@ Module Module1
         Dim path = "C:\Users\Evia\source\repos\VMD\DATA\MOTION.vmd"
         Dim vmd = Reader.OpenAuto(path)
 
+        Call New Xml With {.VMD = vmd}.GetXml.SaveTo("./test.vmd.xml")
+
         Pause()
     End Sub
 End Module
