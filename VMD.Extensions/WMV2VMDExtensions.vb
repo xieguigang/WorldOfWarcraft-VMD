c41c897d6bfb1d7c5a3ae719cacac68234b9b722
xieguigang <xie.guigang@gcmodeller.org>
2018-06-12 00:52:38 +0800

    scale 3D skeleton

--- VMD.Extensions/WMV2VMDExtensions.vb
+++ VMD.Extensions/WMV2VMDExtensions.vb
@@ -1,5 +1,7 @@
 ﻿Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.ComponentModel.Ranges.Model
+Imports Microsoft.VisualBasic.Imaging.Drawing3D
+Imports Microsoft.VisualBasic.Imaging.Drawing3D.Math3D
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Math
 Imports Microsoft.VisualBasic.Math.LinearAlgebra
@@ -15,7 +17,7 @@ Public Module WMV2VMDExtensions
     ''' <param name="skeleton"></param>
     ''' <returns></returns>
     <Extension>
-    Public Function ResetSkeleton(skeleton As skeleton) As skeleton
+    Private Function ResetSkeleton(skeleton As skeleton) As skeleton
         Dim minZ# = skeleton.bones _
                             .Select(Function(b) b.position.z) _
                             .Min
@@ -55,7 +57,7 @@ Public Module WMV2VMDExtensions
     ''' <param name="mmd"></param>
     ''' <returns></returns>
     <Extension>
-    Public Function ResetSkeleton(mmd As PMXFile) As PMXFile
+    Private Function ResetSkeleton(mmd As PMXFile) As PMXFile
         Dim minZ# = mmd.bones.data.Select(Function(b) b.position.z).Min
         Dim xrange As DoubleRange = mmd.bones.data.Select(Function(b) CDbl(b.position.x)).ToArray
         Dim yrange As DoubleRange = mmd.bones.data.Select(Function(b) CDbl(b.position.y)).ToArray
@@ -97,25 +99,44 @@ Public Module WMV2VMDExtensions
             mmd.bones.data.Select(Function(b) CDbl(b.position.y)).ToArray,
             mmd.bones.data.Select(Function(b) CDbl(b.position.z)).ToArray
         }
-        Dim maxScaleX, maxScaleY, maxScaleZ As DoubleRange
+        Dim scaleWOWx, scaleWOWy, scaleWOWz As Double
+        Dim scaleMMDx, scaleMMDy, scaleMMDz As Double
 
         If wowBounds(0).Length > mmdBounds(0).Length Then
-            maxScaleX = wowBounds(0)
+            scaleWOWx = 1
+            scaleMMDx = wowBounds(0).Length / mmdBounds(0).Length
         Else
-            maxScaleX = mmdBounds(0)
+            scaleWOWx = mmdBounds(0).Length / wowBounds(0).Length
+            scaleMMDx = 1
         End If
         If wowBounds(1).Length > mmdBounds(1).Length Then
-            maxScaleY = wowBounds(1)
+            scaleWOWy = 1
+            scaleMMDy = wowBounds(1).Length / mmdBounds(1).Length
         Else
-            maxScaleY = mmdBounds(1)
+            scaleWOWy = mmdBounds(1).Length / wowBounds(1).Length
+            scaleMMDy = 1
         End If
         If wowBounds(2).Length > mmdBounds(2).Length Then
-            maxScaleZ = wowBounds(2)
+            scaleWOWz = 1
+            scaleMMDz = wowBounds(2).Length / mmdBounds(2).Length
         Else
-            maxScaleZ = mmdBounds(2)
+            scaleWOWz = mmdBounds(2).Length / wowBounds(2).Length
+            scaleMMDz = 1
         End If
 
+        Dim o As New Point3D
 
+        For Each bone As bone In wow.bones
+            Dim p3 As New Point3D(bone.position.x, bone.position.y, bone.position.z)
+            p3 = p3.Scale(o, scaleWOWx, scaleWOWy, scaleWOWz)
+            bone.position = New vec3 With {.x = p3.X, .y = p3.Y, .z = p3.Z}
+        Next
+
+        For Each bone In mmd.bones.data
+            Dim p3 As New Point3D(bone.position.x, bone.position.y, bone.position.z)
+            p3 = p3.Scale(o, scaleMMDx, scaleMMDy, scaleMMDz)
+            bone.position = New vec3 With {.x = p3.X, .y = p3.Y, .z = p3.Z}
+        Next
     End Sub
 
     ''' <summary>
@@ -131,6 +152,9 @@ Public Module WMV2VMDExtensions
         Dim mapping As New Dictionary(Of String, String)
 
         Call rescale(wow, mmd)
+
+        wow = wow.ResetSkeleton
+        mmd = mmd.ResetSkeleton
 
         For Each bone As bone In wow.bones
             Dim query As Vector = {
