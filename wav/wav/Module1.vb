eca2682d759cd16d0fbb1f16696c2409e3bda8af
关倩倩 <amethyst.asuka@gcmodeller.org>
2018-08-27 21:10:11 +0800

    wav headers

--- wav/wav/Module1.vb
+++ wav/wav/Module1.vb
@@ -5,7 +5,7 @@ Module Module1
     Sub Main()
 
         Dim wav = "X:\w.wav".OpenBinaryReader
-        Dim header As Header = Header.ParseHeader(wav)
+        Dim header As wave = wave.ParseHeader(wav)
 
 
         Pause()
@@ -13,37 +13,64 @@ Module Module1
 
 End Module
 
-Public Class Header
+Public Class wave
 
-    Public Property Magic As String
+    Public Property magic As String
     Public Property fileSize As Integer
-    Public Property FileType As String
-    Public Property FormatChunkMarker As String
-    Public Property VerifyLength As Integer
-    Public Property Type As Integer
-    Public Property Channels As Integer
-    Public Property SampleRate As Integer
-    Public Property Size As Integer
-    Public Property Size2 As Integer
-    Public Property BitsPerSample As Integer
-    Public Property ChunkHeader As String
-    Public Property DataSize As Integer
+    Public Property format As String
 
-    Public Shared Function ParseHeader(wav As BinaryDataReader) As Header
-        Return New Header With {
-            .Magic = wav.ReadString(4),
+    Public Property fmt As FMTSubChunk
+    Public Property data As DataSubChunk
+
+    Public Shared Function ParseHeader(wav As BinaryDataReader) As wave
+        Return New wave With {
+            .magic = wav.ReadString(4),
             .fileSize = wav.ReadInt32,
-            .FileType = wav.ReadString(4),
-            .FormatChunkMarker = wav.ReadString(4),
-            .VerifyLength = wav.ReadInt32,
-            .Type = wav.ReadInt16,
+            .format = wav.ReadString(4),
+            .fmt = New FMTSubChunk With {
+             .ChunkID = wav.ReadString(4),
+            .ChunkSize = wav.ReadInt32,
+            .audioFormat = wav.ReadInt16,
             .Channels = wav.ReadInt16,
             .SampleRate = wav.ReadInt32,
-            .Size = wav.ReadInt32,
-            .Size2 = wav.ReadInt16,
-            .BitsPerSample = wav.ReadInt16,
-            .ChunkHeader = wav.ReadString(4),
-            .DataSize = wav.ReadInt32
+            .ByteRate = wav.ReadInt32,
+            .BlockAlign = wav.ReadInt16,
+            .BitsPerSample = wav.ReadInt16
+        },
+        .data = DataSubChunk.ParseData(wav)
         }
+    End Function
+End Class
+
+Public Class SubChunk
+
+    Public Property ChunkID As String
+    Public Property ChunkSize As Integer
+
+End Class
+
+Public Class FMTSubChunk : Inherits SubChunk
+    Public Property audioFormat As Integer
+    Public Property Channels As Integer
+    Public Property SampleRate As Integer
+    Public Property ByteRate As Integer
+    Public Property BlockAlign As Integer
+    Public Property BitsPerSample As Integer
+End Class
+
+Public Class DataSubChunk : Inherits SubChunk
+
+    Public Property Data As Integer()
+
+    Public Shared Function ParseData(wav As BinaryDataReader) As DataSubChunk
+        Return New DataSubChunk With {
+            .ChunkID = wav.ReadString(4),
+            .ChunkSize = wav.ReadInt32,
+            .Data = DataSubChunk.loadData(wav).ToArray
+        }
+    End Function
+
+    Private Shared Iterator Function loadData(wav As BinaryDataReader) As IEnumerable(Of Integer)
+
     End Function
 End Class
